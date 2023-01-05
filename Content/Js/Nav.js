317cdb08e8952300e10215d2396ec695a7931e8a
Yusuf bahri karaca <krc.bhr34@gmail.com>
2023-01-05 22:49:27 +0300

    finish

---
+++ Content/Js/Nav.js
@@ -0,0 +1,22 @@
+var Nav = {
+    Elements: {
+        section: document.getElementById("section"),
+
+    },
+    Status: {
+
+    },
+    Actions: {
+        init: () => {
+
+
+        }
+
+
+
+    },
+
+
+
+}
+Nav.Actions.init();
